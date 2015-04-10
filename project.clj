da617390ca050ef9c95a639e4ca1e806897e3c38
Pavel Kopychenko <t34box@gmail.com>
2015-04-10 18:13:40 +0600

    Работа с файлами и рефактроринг

--- project.clj
+++ project.clj
@@ -8,24 +8,30 @@
                  [hiccup "1.0.5"]
                  [clj-time "0.6.0"]
                  [korma "0.3.3"]
+
+                 ;; Нужно только на время разработки
+                 [hiccup-bridge "1.0.1"]
                  ]
+
+  :source-paths ["src" "src-cljs"]
+  
   :profiles
   {:dev
    {:dependencies [
-                   [org.clojure/clojurescript "0.0-3169"]                   
-                   [prismatic/dommy "1.0.0"]                   
-                   [cljs-ajax "0.3.10"]                   
-                   [hipo "0.3.0"]                   
+                   [org.clojure/clojurescript "0.0-3169"]
+                   [prismatic/dommy "1.0.0"]
+                   [cljs-ajax "0.3.10"]
+                   [hipo "0.3.0"]
                    ]
     :plugins [[lein-cljsbuild "1.0.5"]
               [com.cemerick/clojurescript.test "0.3.3"]]}}
   :cljsbuild
   {:builds
-   {:test {:source-paths ["src" "test"]
+   {:test {:source-paths ["src-cljs" "test"]
            :compiler {:output-to "target/unit-test.js"
                       :optimizations :whitespace
                       :pretty-print true}}}
-   
+
    :aliases {"clean-test" ["do" "clean," "cljsbuild" "test"]
              "clean-install" ["do" "clean," "install"]}
    }
