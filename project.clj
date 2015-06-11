0e818a287e2a471c2fa8329df4b74f5fd5f81c1c
povloid <t34box@gmail.com>
2015-06-11 11:42:05 +0600

    Обновление версий библиотек

--- project.clj
+++ project.clj
@@ -6,28 +6,26 @@
   :dependencies [[org.clojure/clojure "1.7.0-beta3"]
                  [compojure "1.3.4"]
                  [hiccup "1.0.5"]
-                 [clj-time "0.6.0"]
+                 [clj-time "0.9.0"]
                  [korma "0.4.2"]
-                 
                  [net.sourceforge.htmlcleaner/htmlcleaner "2.12"]
-
-                 ;; Нужно только на время разработки
-                 ;;[hiccup-bridge "1.0.1"]
                  ]
 
   :source-paths ["src" "src-cljs"]
-  
+
+  :plugins [[lein-ancient "0.6.7"]]
+
   :profiles
   {:dev
    {:dependencies [
-                   [org.clojure/clojurescript "0.0-3269"]
+                   [org.clojure/clojurescript "0.0-3308"]
                    [prismatic/dommy "1.1.0"]
-                   [cljs-ajax "0.3.11"]
+                   [cljs-ajax "0.3.12"]
                    [hipo "0.4.0"]
                    ]
     :plugins [[lein-cljsbuild "1.0.6"]
               [com.cemerick/clojurescript.test "0.3.3"]]}}
-  
+
   :cljsbuild
   {:builds
    {:test {:source-paths ["src-cljs" "test"]
