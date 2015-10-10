040f484e80ec52f5f971656bf5e16ae47671eb0f
pavel <t34box@gmail.com>
2015-10-11 01:07:45 +0600

    Функционал для отображеиня индикатора обращения к сети

--- src-cljs/ixinfestor/net.cljs
+++ src-cljs/ixinfestor/net.cljs
@@ -10,31 +10,43 @@
 
   )
 
+
 (enable-console-print!)
+
+(def progress-element-id "net-in-progress")
 
 (def r (t/reader :json
                  {:handlers
                   {"f" (fn [v] (new js/Number v))
-                   }}
-                 ))
+                   }}))
 
 (def w (t/writer :json))
 (def transit-header (clj->js {:content-type "application/transit+json"}))
 
 (defn get-data [uri params success-fn & [error-fn complete-fn]]
-  (let [req (goog.net.XhrIo.)]
+  (let [req (goog.net.XhrIo.)
+        progress-element (.getElementById js/document progress-element-id)]
 
+    ;; Отобразить элемент прогресса выполнения
+    (when progress-element
+      (set! (-> progress-element .-style .-display) ""))
+    
     (events/listen req goog.net.EventType.ERROR
                    (fn [e]
                      (let [response-text (.getResponseText (.-target e))
                            response (t/read r response-text)]
                        (println "ERROR: " response)
-                       (js/alert (str "ОШИБКА ОБРАЩЕНИЯ К СЕРВЕРУ:\n" response)) 
+                       (js/alert (str "ОШИБКА ОБРАЩЕНИЯ К СЕРВЕРУ:\n" response))
                        (if error-fn (error-fn response)))))
 
     (events/listen req goog.net.EventType.COMPLETE
                    (fn [e]
                      (let [response (t/read r (.getResponseText (.-target e)))]
+
+                       ;; Отобразить элемент прогресса выполнения
+                       (when progress-element
+                         (set! (-> progress-element .-style .-display) "none"))
+                       
                        (println "REQUEST COMPLETE")
                        (if complete-fn (complete-fn response)))))
 
