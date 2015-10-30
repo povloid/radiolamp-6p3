e409a2b608778e91cf585cee10d529d243b0b475
pavel <t34box@gmail.com>
2015-10-30 12:13:05 +0600

    rebranding

--- radiolamp-6P3/project.clj
+++ radiolamp-6P3/project.clj
@@ -1,4 +1,4 @@
-(defproject infestor-clj "0.2.0"
+(defproject radiolamp-6P3 "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
