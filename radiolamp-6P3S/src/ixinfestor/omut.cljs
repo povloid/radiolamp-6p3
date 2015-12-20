6eed8983797b58846ba3cf3c1524ae1f3ee1d375
pavel <t34box@gmail.com>
2015-12-21 05:28:50 +0600

    Добавлено простой групповой элемент для формы

--- radiolamp-6P3S/src/ixinfestor/omut.cljs
+++ radiolamp-6P3S/src/ixinfestor/omut.cljs
@@ -921,6 +921,19 @@
 ;; END
 ;;..................................................................................................
 
+
+
+(defn ui-form-group [{:keys [label label-class+ input-class+ body]
+                      :or {label "метка"
+                           label-class+ "col-xs-12 col-sm-4 col-md-4 col-lg-4"
+                           input-class+ "col-xs-12 col-sm-8 col-md-8 col-lg-8"}}]
+  (dom/div #js {:className "form-group"}
+           (dom/label #js {:className (str "control-label " label-class+) } label)
+           (dom/div #js {:className input-class+ :style #js {}}
+                    body)))
+
+
+
 ;;**************************************************************************************************
 ;;* BEGIN input
 ;;* tag: <input>
