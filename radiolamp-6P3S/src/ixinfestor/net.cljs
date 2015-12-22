372aa9c6aa92e561e06851937852fafe57e2ae12
pavel <t34box@gmail.com>
2015-12-22 13:24:04 +0600

    Исправление

--- radiolamp-6P3S/src/ixinfestor/net.cljs
+++ radiolamp-6P3S/src/ixinfestor/net.cljs
@@ -45,7 +45,7 @@
                      "---------------------------------------------------------\n"
                      (get-response-as-transit-or-text ee)
                      "\n---------------------------------------------------------\n\n")
-            (when (and show-alert (not (empty? (.getStatus ee))))
+            (when (and show-alert  (not= 0 (.getStatus ee)))
               (js/alert (str
                          "ERROR IN [" s "]\n"
                          "Status: " (.getStatus ee) " - " (.getStatusText ee) "\n"
