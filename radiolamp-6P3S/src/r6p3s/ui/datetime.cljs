10c2d806256f483c3df1986b678e64c0cfe5aede
pavel <t34box@gmail.com>
2016-04-05 23:45:24 +0600

    Исправление после рефакторинга datatime -> datetime

---
+++ radiolamp-6P3S/src/r6p3s/ui/datetime.cljs
@@ -0,0 +1,13 @@
+(ns r6p3s.ui.datetime
+  (:require [om.dom :as dom :include-macros true]
+            [r6p3s.core :as rc]
+            [r6p3s.ui.glyphicon :as gicon]))
+
+
+
+
+(defn render [d]
+  (dom/span
+   #js {:className "text-muted"}
+   (gicon/render "time") " "
+   (rc/date-com-format-datetime d)))
