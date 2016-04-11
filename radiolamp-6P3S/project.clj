911e7f6b6e56d0225b8eef7da92ea94c2c2e7f2f
pavel <t34box@gmail.com>
2016-04-11 22:12:08 +0600

    перенос  cljsjs/d3 в бибилиотеку r6p3s

--- radiolamp-6P3S/project.clj
+++ radiolamp-6P3S/project.clj
@@ -3,13 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
-
-                 ;; CLOJURESCRIPT LIBRARIES
-                 [org.clojure/clojurescript "1.8.40"]
+  :dependencies [[org.clojure/clojurescript "1.8.40"]
                  [com.cognitect/transit-cljs "0.8.237"]
                  [org.omcljs/om "0.9.0"]
                  [secretary "1.2.3"]
-
-                 ]
-  )
+                 [cljsjs/d3 "3.5.7-1"]])
