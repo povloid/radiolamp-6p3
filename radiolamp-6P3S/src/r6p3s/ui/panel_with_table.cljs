88414a1407480b145bcc99b1d97d10d89f73a549
pavel <t34box@gmail.com>
2016-09-09 18:26:05 +0600

    Доработка компонента отображения таблиц-панелей

--- radiolamp-6P3S/src/r6p3s/ui/panel_with_table.cljs
+++ radiolamp-6P3S/src/r6p3s/ui/panel_with_table.cljs
@@ -7,29 +7,39 @@
 
 
 
+
 (defn render [{:keys [cols rows
                       striped? bordered? hover? responsive?]
-               :or   {cols        [] rows []
+               :or   {cols        []
+                      rows        []
                       striped?    true
                       bordered?   true
                       hover?      true
                       responsive? true}
                :as   options}]
-  (panel/render
-   (assoc options
-          :after-body
-          (table/render
-           {:hover?      hover?
-            :bordered?   bordered?
-            :striped?    striped?
-            :responsive? responsive?
-            :thead       (->> cols
-                              (map #(dom/th nil %))
-                              thead-tr/render)
-            :tbody       (->> rows
-                              (map (fn [row]
-                                     (apply
-                                      dom/tr nil
-                                      (vec (map #(dom/td nil %) row)))))
-                              (apply dom/tbody nil))
-            }))))
+  (let [empty-cols? (empty? cols)
+        t-width (when empty-cols?
+                  (when-let [c (first rows)]
+                    (str (quot 100 (count c)) "%")))]
+    (panel/render
+     (assoc options
+            :after-body
+            (table/render
+             {:hover?      hover?
+              :bordered?   bordered?
+              :striped?    striped?
+              :responsive? responsive?
+              :thead       (->> cols
+                                (map #(dom/th #js {:width t-width} %))
+                                thead-tr/render)
+              :tbody       (->> rows
+                                (map (fn [row]
+                                       (apply
+                                        dom/tr nil
+                                        (vec (map #(dom/td
+                                                    (when empty-cols?
+                                                      #js {:width t-width})
+                                                    %)
+                                                  row)))))
+                                (apply dom/tbody nil))
+              })))))
