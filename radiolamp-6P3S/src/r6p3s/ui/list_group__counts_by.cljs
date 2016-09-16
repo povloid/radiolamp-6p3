0e50c18d90eef9523206eed9f1e62e68b2645e8c
pavel <t34box@gmail.com>
2016-09-16 12:55:37 +0600

    Сортировка в list-group--counts-by

--- radiolamp-6P3S/src/r6p3s/ui/list_group__counts_by.cljs
+++ radiolamp-6P3S/src/r6p3s/ui/list_group__counts_by.cljs
@@ -5,11 +5,21 @@
 
 
 (defn render [points f
-              {:keys [text nil-text]
+              {:keys [text nil-text order-fn order-direction]
                :or   {text     "метка в заголовке"
                       nil-text "не указано"}}]
-  (let [items (->> points (group-by f) seq)]
+  (let [items (->> points
+                   (group-by f)
+                   seq)
+
+        items (if order-fn
+                (let [v (sort-by (comp order-fn second) items)]
+                  (if (= order-direction :desc)
+                    (reverse v)
+                    v))                
+                items)]
     (list-group/render
+     
      (reduce
       (fn [a [gi items]]
         (conj a (list-group-item/render
