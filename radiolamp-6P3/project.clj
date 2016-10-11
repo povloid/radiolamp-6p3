d5ffb0c566c772c0ef8229204edcc3346d0e03c2
pavel <t34box@gmail.com>
2016-10-11 14:07:02 +0600

    Исправление..

--- radiolamp-6P3/project.clj
+++ radiolamp-6P3/project.clj
@@ -20,7 +20,7 @@
                  [clj-time "0.12.0"]
 
                  ;;[org.postgresql/postgresql "9.4.1211"]
-                 [postgresql "9.4-1206-jdbc41"]
+                 [org.postgresql/postgresql "9.4-1206-jdbc41"]
                  [korma "0.4.2"]
                  
 
