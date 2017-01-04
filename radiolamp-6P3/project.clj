8d4bc44fbd8e77d84966a178c6308b3e8987689c
pavel <t34box@gmail.com>
2017-01-04 11:31:06 +0600

    Рефакторинг ixinfestor -> r6p3

--- radiolamp-6P3/project.clj
+++ radiolamp-6P3/project.clj
@@ -1,32 +1,32 @@
-(defproject radiolamp-6P3 "0.3.0"
+(defproject radiolamp-6P3 "0.4.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.391"]
+                 [org.clojure/core.async "0.2.395"]
 
                  [ring "1.5.0"]
                  [ring/ring-json "0.4.0"]
 
                  ;;[ring-transit "0.1.3"]
                  [prismatic/plumbing "0.5.3"] ;; Просит транзит
-                 [com.cognitect/transit-clj "0.8.290"]
+                 [com.cognitect/transit-clj "0.8.297"]
 
                  [compojure "1.5.1"]
                  [hiccup "1.0.5"]
                  [com.cemerick/friend "0.2.3"]
 
-                 [clj-time "0.12.0"]
+                 [clj-time "0.13.0"]
 
-                 ;;[org.postgresql/postgresql "9.4.1211"]
-                 [org.postgresql/postgresql "9.4-1206-jdbc41"]
+                 [org.postgresql/postgresql "9.4.1212"]
+                 ;;[org.postgresql/postgresql "9.4-1206-jdbc41"]
                  [korma "0.4.2"]
                  
 
-                 [net.sourceforge.htmlcleaner/htmlcleaner "2.16"]
+                 [net.sourceforge.htmlcleaner/htmlcleaner "2.18"]
 
-                 [com.draines/postal "1.11.4"]
+                 [com.draines/postal "2.0.2"]
                  [image-resizer "0.1.9"]
                  ]
 
--- radiolamp-6P3S/project.clj
+++ radiolamp-6P3S/project.clj
@@ -1,11 +1,11 @@
-(defproject radiolamp-6P3S "0.3.0"
+(defproject radiolamp-6P3S "0.4.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojurescript "1.9.229"]
+  :dependencies [[org.clojure/clojurescript "1.9.293"]
                  [com.cognitect/transit-cljs "0.8.239"]
                  [org.omcljs/om "0.9.0"]
                  [secretary "1.2.3"]
                  [cljsjs/d3 "3.5.16-0"]
-                 [markdown-clj "0.9.89"]])
+                 [markdown-clj "0.9.91"]])
