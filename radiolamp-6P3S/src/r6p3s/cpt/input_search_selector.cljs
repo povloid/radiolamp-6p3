672b231a08843e9486befb416e02daf36d29f96f
pavel <t34box@gmail.com>
2017-01-13 16:07:50 +0600

    Доработка компонента

--- radiolamp-6P3S/src/r6p3s/cpt/input_search_selector.cljs
+++ radiolamp-6P3S/src/r6p3s/cpt/input_search_selector.cljs
@@ -16,13 +16,9 @@
 
 
 
-(def app-init
-  {:selected nil
-   :input    input/app-init
-   :data     []})
 
 
-
+;; -------------------------------------------------------------------------------------------
 ;; TODO вынести данную функцию в общий функционал
 (defn select-step [ks data]
   (let [data (vec data)]
@@ -43,6 +39,30 @@
        (select-step ks)
        reverse
        vec))
+;; -------------------------------------------------------------------------------------------
+
+
+
+
+(def app-init
+  {:selected nil
+   :input    input/app-init
+   :data     []})
+
+
+
+(defn get-selected [app]
+  (:selected app))
+
+
+(defn set-selected
+  ([app row]
+   (set-selected app row {:text-fn :keyname}))
+  ([app row {:keys [text-fn]}]
+   (-> app
+       (assoc :selected row)
+       (update-in [:input] input/set-value! (text-fn row)))))
+
 
 (def select-step-omut (partial select-step [:omut-row :selected]))
 (def select-step-next-omut (partial select-step-next [:omut-row :selected]))
@@ -54,192 +74,200 @@
 (defn component
   [app own
    {:keys [class+
-           search-data-fn]
-    :or   {search-data-fn (fn [chan-ret-data text]
-                            (println '>>> text)
-                            (->> {}
-                                 (repeat 10)
-                                 (map-indexed
-                                  (fn [i row]
-                                    (assoc row :text (str text " " i))))
-                                 vec
-                                 (put! chan-ret-data)))}
+           search-data-fn
+           text-fn
+           ui-row-fn]
+    :or   {}
     :as   opts}]
 
-  (reify
-    om/IInitState
-    (init-state [_]
-      {:chan-update   (chan)
-       :chan-ret-data (chan)
-       :in-progress?  false
-       :show-popup?   false})
+  (let [text-fn        :keyname
+        search-data-fn (or search-data-fn
+                           (fn [chan-ret-data text]
+                             (println '>>> text)
+                             (->> {}
+                                  (repeat 10)
+                                  (map-indexed
+                                   (fn [i row]
+                                     (assoc row text-fn (str text " " i))))
+                                  vec
+                                  (put! chan-ret-data))))
+        ui-row-fn      (or ui-row-fn
+                           (fn [row]
+                             [(dom/td nil (text-fn @row))]))]
 
-    om/IWillMount
-    (will-mount [this]
-      (let [{:keys [chan-update
-                    chan-ret-data]} (om/get-state own)]
+    (reify
+      om/IInitState
+      (init-state [_]
+        {:chan-update   (chan)
+         :chan-ret-data (chan)
+         :in-progress?  false
+         :show-popup?   false})
 
-        (go
-          (while true
-            (let [_ (<! chan-update)]
-              )))
+      om/IWillMount
+      (will-mount [this]
+        (let [{:keys [chan-update
+                      chan-ret-data]} (om/get-state own)]
 
-        (go
-          (while true
-            (let [data (<! chan-ret-data)]
-              (om/update! app :data data)
-              (om/set-state! own :in-progress? false))))
+          (go
+            (while true
+              (let [_ (<! chan-update)]
+                )))
 
-        (put! chan-update 1)))
+          (go
+            (while true
+              (let [data (<! chan-ret-data)]
+                (om/update! app :data data)
+                (om/set-state! own :in-progress? false))))
 
-    om/IRenderState
-    (render-state [_ {:keys [chan-update chan-ret-data
-                             in-progress? show-popup?]}]
-      (letfn [(search []
-                (when-not in-progress?
-                  (do
-                    (om/set-state! own :in-progress? true)
-                    (om/set-state! own :show-popup? true)
-                    (->> @app
-                         :input
-                         input/value
-                         (search-data-fn chan-ret-data)))))
+          (put! chan-update 1)))
 
-              (get-selected []
-                (->> @app
-                     :data
-                     (filter #(get-in % [:omut-row :selected]))
-                     first))
+      om/IRenderState
+      (render-state [_ {:keys [chan-update chan-ret-data show-popup?]}]
+        (letfn [(search []
+                  (om/set-state! own :show-popup? true)
+                  (->> @app
+                       :input
+                       input/value
+                       (search-data-fn chan-ret-data)))
 
-              (open []
-                (om/set-state! own :show-popup? true))
+                (get-selected []
+                  (->> @app
+                       :data
+                       (filter #(get-in % [:omut-row :selected]))
+                       first))
 
-              (close []
-                (om/set-state! own :show-popup? false))
+                (open []
+                  (om/set-state! own :show-popup? true))
 
-              (clear []
-                (om/set-state! own :show-popup? false)
-                (om/update! app app-init))
+                (close []
+                  (om/set-state! own :show-popup? false))
 
-              (up-select []
-                (om/set-state! own :show-popup? true)
-                (om/transact! app :data select-step-omut))
-              (down-select []
-                (om/set-state! own :show-popup? true)
-                (om/transact! app :data select-step-next-omut))
+                (clear []
+                  (om/set-state! own :show-popup? false)
+                  (om/update! app app-init))
 
-              (select-value []
-                (if (om/get-state own :show-popup?)
-                  (do
-                    (om/transact!
-                     app
-                     (fn [app]
-                       (let [selected (get-selected)]
-                         (if (empty? selected)
-                           app
-                           (-> app
-                               (assoc :selected selected)
-                               (update-in [:input] input/set-value! (:text selected)))))))
-                    (om/set-state! own :show-popup? false))
-                  ;; Или повторно открываем поиск...
-                  (open)))]
-        (let [app-v @app]
-          (dom/div
-           nil
-           (dom/div
-            #js {:className (str "input-group " class+) :style #js {:marginBottom 6}}
+                (up-select []
+                  (om/set-state! own :show-popup? true)
+                  (om/transact! app :data select-step-omut))
+                (down-select []
+                  (om/set-state! own :show-popup? true)
+                  (om/transact! app :data select-step-next-omut))
 
-            (dom/span #js {:className "input-group-btn"}
-                      (button/render {:type     :default
-                                      :on-click clear
-                                      :text     (glyphicon/render "remove")}))
+                (select-value []
+                  (if (om/get-state own :show-popup?)
+                    (do
+                      (om/transact!
+                       app (fn [app]
+                             (let [selected (get-selected)]
+                               (if (empty? selected)
+                                 app (set-selected app selected {:text-fn text-fn})))))
+                      (om/set-state! own :show-popup? false))
+                    ;; Или повторно открываем поиск...
+                    (open)))]
+          (let [app-v @app]
+            (dom/div
+             nil
+             (dom/div
+              #js {:className (str "input-group " class+) :style #js {:marginBottom 6}}
 
-
-            (om/build input/component (app :input)
-                      {:opts {:placeholder "Введите текст для поиска..."
-                              :onKeyDown-fn
-                              (fn [v]
-                                (let [kod (.-which v)]
-                                  (condp = kod
-                                    13 (do (select-value)
-                                           (.preventDefault v))
-                                    38 (do (up-select)
-                                           (.preventDefault v))
-                                    40 (do (down-select)
-                                           (.preventDefault v))
-                                    27 (do (close)
-                                           (.preventDefault v))
-                                    (println "key:" kod)))
-                                1)
-
-                              :onChange-updated-fn search}})
+              (dom/span #js {:className "input-group-btn"}
+                        (button/render {:type     :default
+                                        :on-click clear
+                                        :text     (glyphicon/render "remove")}))
 
 
-            (dom/span #js {:className "input-group-btn"}
-                      (button/render {:type     :success
-                                      :active?  show-popup?
-                                      :on-click open
-                                      :text     (glyphicon/render "search")})))
+              (om/build input/component (app :input)
+                        {:opts {:placeholder         "Введите текст для поиска..."
+                                :onKeyDown-fn
+                                (fn [v]
+                                  (let [kod (.-which v)]
+                                    (condp = kod
+                                      13 (do (select-value)
+                                             (.preventDefault v))
+                                      38 (do (up-select)
+                                             (.preventDefault v))
+                                      40 (do (down-select)
+                                             (.preventDefault v))
+                                      27 (do (close)
+                                             (.preventDefault v))
+                                      (do (println "key:" kod))))
+                                  1)
+                                :onChange-updated-fn search}})
 
-           (let [[icon text-class] (if (= (get-in app-v [:selected :text])
-                                          (get-in app-v [:input :value]))
-                                     ["ok"       "text-success"]
-                                     ["asterisk" "text-warning"])]
-             (dom/div #js {:className text-class
-                           :style     #js {:position "absolute"
-                                           :top      8
-                                           :right    65
-                                           :zIndex   5}}
-                      (glyphicon/render icon)))
+
+              (dom/span #js {:className "input-group-btn"}
+                        (button/render {:type     :success
+                                        :active?  show-popup?
+                                        :on-click open
+                                        :text     (glyphicon/render "search")})))
+
+             (let [[icon text-class] (if (= (get-in app-v [:selected text-fn])
+                                            (get-in app-v [:input :value]))
+                                       ["ok"       "text-success"]
+                                       ["asterisk" "text-warning"])]
+               (dom/div #js {:className text-class
+                             :style     #js {:position "absolute"
+                                             :top      8
+                                             :right    65
+                                             :zIndex   5}}
+                        (glyphicon/render icon)))
 
 
-           (dom/div
-            #js {:style #js {:display    (if show-popup? "" "none")
-                             :position   "absolute"
-                             :width      "100%"
-                             :zIndex     1
-                             :boxShadow  "0px 2px 8px"
-                             :background "white"}}
-            (table/render {:hover?      true
-                           :bordered?   true
-                           :striped?    true
-                           :responsive? false
-                           :tbody
-                           (om/build tbody-trs-sel/component  (:data app)
-                                     {:opts {:selection-type :one
-                                             :app-to-tds-seq-fn
-                                             (fn [row]
-                                               (let [{:keys [text]} @row]
-                                                 [(dom/td nil text)]))
-                                             :on-select-fn
-                                             (fn [{:keys [] :as row}])
-                                             }})
-                           })
+             (dom/div
+              #js {:style #js {:display    (if show-popup? "" "none")
+                               :position   "absolute"
+                               :width      "100%"
+                               :zIndex     1
+                               :boxShadow  "0px 2px 8px"
+                               :background "white"}}
+              (table/render {:hover?      true
+                             :bordered?   true
+                             :striped?    true
+                             :responsive? false
+                             :tbody
+                             (om/build tbody-trs-sel/component  (:data app)
+                                       {:opts {:selection-type    :one
+                                               :app-to-tds-seq-fn ui-row-fn
+                                               ;;:on-select-fn
+                                               ;;(fn [{:keys [] :as row}])
+                                               }})
+                             })
 
-            (dom/div #js {:className "btn-group"
-                          :style     #js {:float  "right"
-                                          :margin 4}}
+              (dom/div #js {:className "btn-group"
+                            :style     #js {:float  "right"
+                                            :margin 4}}
 
-                     (button/render
-                      {:type     :success
-                       :on-click search
-                       :text     (dom/span nil
-                                           (glyphicon/render "search")
-                                           " Поиск")})
+                       (button/render
+                        {:type     :success
+                         :on-click search
+                         :text     (dom/span nil
+                                             (glyphicon/render "search")
+                                             " Поиск")})
 
-                     (button/render
-                      {:type      :primary
-                       :on-click  select-value
-                       :disabled? (empty? (get-selected))
-                       :text      (dom/span nil
-                                            (glyphicon/render "ok")
-                                            " Выбрать")})
-                     (button/render
-                      {:type     :default
-                       :on-click close
-                       :text     (dom/span nil
-                                           (glyphicon/render "remove")
-                                           " Закрыть")})))))))))
+                       (button/render
+                        {:type      :primary
+                         :on-click  select-value
+                         :disabled? (empty? (get-selected))
+                         :text      (dom/span nil
+                                              (glyphicon/render "ok")
+                                              " Выбрать")})
+                       (button/render
+                        {:type     :default
+                         :on-click close
+                         :text     (dom/span nil
+                                             (glyphicon/render "remove")
+                                             " Закрыть")})))
+
+             ;; Отображаем выбраное
+             (if (app-v :selected)
+               (->> app
+                    :selected
+                    ui-row-fn
+                    (apply dom/tr nil)
+                    (dom/tbody nil)
+                    (dom/table #js {:style #js {:width "100%"}}))
+               (dom/p #js {:className "text-mutted"}
+                      "нет.")))))))))
 
 
 
