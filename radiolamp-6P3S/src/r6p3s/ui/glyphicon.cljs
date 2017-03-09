32d778911b2ce40ee952fcdefd02d84c8271a0ef
pavel <t34box@gmail.com>
2017-03-09 21:14:21 +0600

    Задание стиля для иконок от бутстрапа

--- radiolamp-6P3S/src/r6p3s/ui/glyphicon.cljs
+++ radiolamp-6P3S/src/r6p3s/ui/glyphicon.cljs
@@ -1,8 +1,10 @@
 (ns r6p3s.ui.glyphicon
   (:require [om.dom :as dom :include-macros true]))
 
-(defn render [name & [class+ size]]
+(defn render [name & [class+ size style]]
   (dom/span #js {:className   (str "glyphicon glyphicon-"
                                    name " " (or class+ ""))
-                 :style       #js {:fontSize size}
+                 :style       (if size
+                                #js {:fontSize size}
+                                style)
                  :aria-hidden "true"}))
