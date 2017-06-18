e3cc024003e8addc9accce3dcc7940f700a0a56f
pavel <t34box@gmail.com>
2017-06-18 12:33:53 +0600

    Рефакторинг - разбивка серверной части - функционал dataform вынесен в отдельную библиотеку radiolamp-r6p3-complex-dataform

--- build.sh
+++ build.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
+
 cd radiolamp-6p3
 lein clean
 lein install
+cd -
 
-cd ../radiolamp-6p3s
+cd radiolamp-6p3-complex-dataform
 lein clean
 lein install
+cd -
 
-cd ../radiolamp-6p3s-complex-dataform
+cd radiolamp-6p3s
 lein clean
 lein install
+cd -
 
-cd ../radiolamp-6p3s-d3js
+cd radiolamp-6p3s-complex-dataform
 lein clean
 lein install
+cd -
 
-cd ../radiolamp-6p3s-yandex
+cd radiolamp-6p3s-d3js
 lein clean
 lein install
+cd -
 
-cd ..
+cd radiolamp-6p3s-yandex
+lein clean
+lein install
+cd -
---
+++ radiolamp-6p3-complex-dataform/project.clj
@@ -0,0 +1,9 @@
+(defproject radiolamp-6p3-complex-dataform "0.1.0"
+  :description "Server side for dataform"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[radiolamp-6p3 "0.5.0"]]
+
+  :aot :all
+  :omit-source true)
